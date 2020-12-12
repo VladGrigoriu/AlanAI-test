319951b1aba656196b9396f9f5b8fb998ee54cd4
Vlad Grigoriu <vlad.grigoriu4@gmail.com>
2020-12-12 15:47:31 +0100

    alanAI-test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React,{ useEffect } from 'react'
 import './App.css';
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
+  useEffect(() => {
+    alanBtn({
+        key: '1d74f852c2c609de7d482cd0acb300c02e956eca572e1d8b807a3e2338fdd0dc/stage',
+        onCommand: (commandData) => {
+            if (commandData.command === 'go:back') {
+             
+                }
+            }
+    });
+}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       
+
     </div>
+    
   );
 }
 
